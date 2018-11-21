86168f08b13fbba0d4a38982b653d6446dc9c618
Sammy <kmsium@gmial.com>
2018-11-21 03:44:27 -0800

    Initial Commit

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
-const express=require('express');
-const bodyParser=require('body-parser');
-const app=express();
-import router from './config/routes';
-const server_port=4500; //change as necessary
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+import router from "./config/routes";
 
-
+const server_port = process.env.PORT || 4500;
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-app.use('/api',router);
+app.use("/api", router);
 
-app.listen(server_port, ()=>{
-    console.log(`I am running on port ${server_port} port`)
+app.listen(server_port, () => {
+  console.log(`I am running on port ${server_port} port`);
 });
